1ad9b93a38314c6d0108c1fb8f841d372596f155
Tetrax <jamesjakait48@gmail.com>
2024-02-15 10:55:55 +0300

    version 2

--- includes/config_session.inc.php
+++ includes/config_session.inc.php
@@ -12,26 +12,27 @@ session_set_cookie_params([
 session_start();
 // creates session in php
 
-if(isset($_SESSION['user_id'])){
+if(isset($_SESSION['user_id'])){ //creates the session upon login
     if(!isset($_SESSION['last_regeneration'])){
         regenerate_session_id();
-    }else{
+    }else{                         // if there was already a session the session should last for 30 minutes
         $interval = 60 * 30;
-        if(time() - $_SESSION['last_regeneration'] >= $interval){
+        if(time() - $_SESSION['last_regeneration'] >= $interval){ // if session is more than 30 minutes create a new session
         regenerate_session_id();
         }
     }
     }else{
-        if(!isset($_SESSION['last_regeneration'])){
+        if(!isset($_SESSION['last_regeneration'])){ //if the session expires then create a new session using user id.
             regenerate_session_id_loggedin();
         }else{
             $interval = 60 * 30;
-            if(time() - $_SESSION['last_regeneration'] >= $interval){
+            if(time() - $_SESSION['last_regeneration'] >= $interval){ // if session lasted more than 30 minutes create a new session using user id 
             regenerate_session_id_loggedin();
             }
         }
     }
-function regenerate_session_id_loggedin(){
+//function to create sessions using user id
+    function regenerate_session_id_loggedin(){
         session_regenerate_id();
 
     $user_id = $_SESSION["user_id"];
@@ -39,7 +40,9 @@ function regenerate_session_id_loggedin(){
     $sessionid = $newSessionid . "_" . $user_id;
     session_id($sessionid);
         $_SESSION["last_regeneration"] = time();
+
     }
+    //function to regenerate user id if session expires
 function regenerate_session_id(){
     session_regenerate_id();
     $_SESSION["last_regeneration"] = time();
